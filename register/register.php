b9e7b7654d914e3a5be3dbaf43c12cf913fe65b8
maulana <maulana.alirridlo@gmail.com>
2020-01-29 19:16:07 +0700

    5_register_succes

--- register/register.php
+++ register/register.php
@@ -1,35 +1,22 @@
 <?php
 require_once("../connection.php");
-var_dump($db);
+// var_dump($db);
 
 if(isset($_POST['register'])){
-    // filter data yang diinputkan
-    // $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    // enkripsi password
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
     // $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-
-
-    // menyiapkan query
-    $sql = "INSERT INTO users (username, password) 
-            VALUES (:username, :password)";
-    $stmt = $connect->prepare($sql);
-
-    // bind parameter ke query
-    $params = array(
-        // ":name" => $name,
-        ":username" => $username,
-        ":password" => $password,
-        // ":email" => $email
-    );
-
-    // eksekusi query untuk menyimpan ke database
-    $saved = $stmt->execute($params);
-
-    // jika query simpan berhasil, maka user sudah terdaftar
-    // maka alihkan ke halaman login
-    if($saved) header("Location: login.php");
+    $cekuser = mysqli_query($connect, "SELECT * FROM user WHERE username = '$username'");
+    if(mysqli_num_rows($cekuser) > 0) {
+        print "<script>alert('username sudah ada!');window.location='register.html'</script>";
+    }
+    elseif($_POST["password"] != $_POST["confirm"]){
+        print "<script>alert('confirm password dan password harus sama!');window.location='register.html'</script>";
+    }
+    else{
+        $query = mysqli_query($connect, "INSERT INTO user (username, password)
+                 values ('$username','$password');");
+        if($query) header("Location: ../login/login.html");
+    }
 }
-
 ?>

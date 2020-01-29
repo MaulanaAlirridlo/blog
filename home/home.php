69fc0f7e77425bafa983f92c74dcefc0f765d329
maulana <maulana.alirridlo@gmail.com>
2020-01-29 12:56:31 +0700

    4

---
+++ home/home.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("cek.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pesbuk Timeline</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <div id="background">
+        <div id="post">
+            <a href="../post/post.html"><button>post</button></a>
+        </div>
+    </div>
+</body>
+</html>
--- post/post.php
+++ post/post.php
@@ -1,15 +1,3 @@
 <?php
     include'../connection.php'
 ?>
-<html>
-    <head>
-        <title>post</title>
-        <link rel="stylesheet" href="./post.css">
-    </head>
-    <body>
-        <div id="post">
-            <label>POST</label><br>     <textarea></textarea><br>
-            <input type="submit" action="post" Value="Kirim">
-        </div>
-    </body>
-</html>
